65916ee5461841806a1b343b2491d61a52b6164e
Dejan Mitrovic <dejan.mitrovic112@gmail.com>
2021-10-15 23:09:00 +0200

    Added eslint, removed prettier, and implemented test component to check if eslint is working. Added settings import/resolver to eslintrc.json so imports arent showing wrong path error. Also added eslintignore for node_modules and dist. Rules for eslint I will update in run if i find it necessary. Configured airbnb for coding guidelines.

--- workpuls-app/src/app/app.module.ts
+++ workpuls-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +14,9 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
